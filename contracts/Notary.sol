5fc498a6662755267689671756b8b089b18eaed2
Stefan Beyer <stefan@cryptonics.consulting>
2021-03-13 23:42:41 +0100

    ZK authorship claims

--- contracts/Notary.sol
+++ contracts/Notary.sol
@@ -7,34 +7,28 @@ contract Notary {
   struct Record{
     uint256 timestamp;
     bytes signature;
+    string ZkAuthorshipClaim;
   }
 
   mapping (bytes32 => Record) records;
   
-  function getRecord(bytes32 _hash) public view returns(uint256 timestamp, bytes memory sig) {
+  function getRecord(bytes32 _hash) public view returns(uint256 timestamp, bytes memory sig, string memory claim) {
     timestamp = records[_hash].timestamp;
     sig = records[_hash].signature;
+    claim = records[_hash].ZkAuthorshipClaim;
   }
 
   
-  function setRecord(bytes32 _hash, bytes memory sig) public {
+  function setRecord(bytes32 _hash, bytes memory sig, string memory claim) public {
     require(records[_hash].timestamp == 0,"This document has already been recorded");
     require(sig.length == 0 || sig.length == 65, "Signature must be emptry or 65 bytes"); 
     records[_hash].signature = sig;
+    records[_hash].ZkAuthorshipClaim = claim;
     records[_hash].timestamp = block.timestamp;
+    records[_hash].ZkAuthorshipClaim = claim;
   }
 
 
-  function getRecordAndSigner(bytes32 _hash) public view returns(uint256 timestamp, address signer) {
-    timestamp = records[_hash].timestamp;
-    bytes memory sig = records[_hash].signature;
-
-    if (timestamp != 0) {
-        bytes32 prefixedHash = ECDSA.toEthSignedMessageHash(_hash);
-        signer = ECDSA.recover(prefixedHash, sig);
-    }
-
-  }  
 
 
 }
